60c6d4e8d92b2852591a43db0c77794f71a4d05e
Samuel Mercado <samuel.mg@gmx.com>
2023-07-05 17:07:36 -0600

    Actualiza CondominioSeeder para generar condominio con viviendas

--- database/seeders/CondominioSeeder.php
+++ database/seeders/CondominioSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Condominio;
 use App\Models\User;
+use App\Models\Vivienda;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,8 +16,16 @@ class CondominioSeeder extends Seeder
     public function run(): void
     {
         Condominio::factory()
-            ->hasAttached(User::factory(), ['role' => 'Administrador'])
-            ->count(3)
+            ->hasAttached(
+                User::factory()
+                    ->state(function (array $attributes) {
+                        return ['email' => 'admin@test.com'];
+                    })
+                    ->withPersonalTeam(),
+                ['role' => 'Administrador']
+            )
+            ->has(Vivienda::factory()->count(30))
+            // ->count(1)
             ->create();
     }
 }
