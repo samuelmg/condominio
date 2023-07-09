01d806554bba1e00bc19f7ae355a40e4815d43af
Samuel Mercado <samuel.mg@protonmail.com>
2023-07-09 15:08:33 -0600

    Corrige componente de livewire

--- resources/views/livewire/persona/persona-form.blade.php
+++ resources/views/livewire/persona/persona-form.blade.php
@@ -1,11 +1,8 @@
-<div x-data="{ modelOpen: false }">
-    <button @click="modelOpen =!modelOpen" class="flex items-center justify-center px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
-        <svg class="w-5 h-5 mx-1.5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
-        </svg>
-    
-        <span class="mx-1.5">Agregar Persona</span>
-    </button>
+<div
+    x-data="{ modelOpen: false }"
+    @keydown.escape.window="modelOpen = false"
+    @open-persona-form-modal.window="modelOpen = true"
+>
 
     <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
@@ -42,10 +39,10 @@
                     Las personas agregadas podrán crear una cuenta de usuario en el sistema.
                 </p>
 
-                <form class="mt-5">
+                <form wire:submit.prevent="save" method="POST" class="mt-5">
                     <div>
                         <x-form.input
-                            wire:model.lazy="nombre"
+                            wire:model.defer="nombre"
                             name="nombre"
                             placeholder="Nombre de la persona"
                             value="{{ old('nombre') ?? (isset($persona) ? $persona->nombre : '') }}"
@@ -54,20 +51,18 @@
 
                     <div class="mt-4">
                         <x-form.input 
-                            wire.model.lazy="tel_celular"
-                            name="tel_celular"
+                            wire:model.defer="tel_celular"
                             label="Teléfono Celular"
                             placeholder="Teléfono a 10 dígitos (sin comas, guiones o espacios)"
-                            type="tel"
-                            pattern="[0-9]{10}"
+                            {{-- type="tel"
+                            pattern="[0-9]{10}" --}}
                             value="{{ old('tel_celular') ?? (isset($persona) ? $persona->tel_celular : '') }}"
                         />
                     </div>
 
                     <div class="mt-4">
                         <x-form.input
-                            wire.model.lazy="tel_fijo"
-                            name="tel_fijo"
+                            wire:model.defer="tel_fijo"
                             label="Teléfono Fijo"
                             placeholder="(Opcional)"
                             type="tel"
@@ -78,8 +73,7 @@
 
                     <div class="mt-4">
                         <x-form.select
-                            wire.model.lazy="vivienda_id"
-                            name="vivienda_id"
+                            wire:model.defer="vivienda_id"
                             label="No. Vivienda"
                             agregar-valor-vacio=true
                             :options="$viviendas"
@@ -89,8 +83,8 @@
                     </div>
                     
                     <div class="mt-4">
-                        <x-form.radio name="tipo" label="Inquilino" value="Inquilino" :checked-value="isset($persona) ? $persona->viviendas->first()->pivot->tipo : null" />
-                        <x-form.radio name="tipo" label="Dueño" value="Dueño" class="mt-4" :checked-value="isset($persona) ? $persona->viviendas->first()->pivot->tipo : null" />
+                        <x-form.radio wire:model.defer="tipo" label="Inquilino" value="Inquilino" :checked-value="isset($persona) ? $persona->viviendas->first()->pivot->tipo : null" />
+                        <x-form.radio wire:model.defer="tipo" label="Dueño" value="Dueño" class="mt-4" :checked-value="isset($persona) ? $persona->viviendas->first()->pivot->tipo : null" />
                         @error('tipo')
                             <span class="text-xs text-red-600 dark:text-red-400">
                                 {{ $message }}
@@ -101,7 +95,7 @@
                     <div class="flex justify-end mt-6">
                         {{-- <x-form.button type="button" wire:click.prevent="save()" texto="Guardar">
                         </x-form.button> --}}
-                        <button type="submit" wire:click.prevent="save()" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                        <button class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
                             Guardar
                         </button>
                     </div>
